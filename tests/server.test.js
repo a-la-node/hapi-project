daf4a8c9113b3a0a410da782552b4cde1ffffded
Cleop <cleo.pearson@hotmail.co.uk>
2016-11-22 11:25:57 +0000

    Tests for handlebars helper Spy user in index.html response

--- tests/server.test.js
+++ tests/server.test.js
@@ -21,6 +21,7 @@ test('Check the response content is received from the server', t => {
   server.inject(options, (response) =>{
   t.equal(response.statusCode, 200, 'You received a 200 status code');
   t.notEqual(response.payload.indexOf('<h1> DISGUISED </h1>'), -1, 'The h1 title was found in the index.html response');
+  t.notEqual(response.payload.indexOf("<a href='results?type=spy'><li>Spy</li></a>"), -1, 'The handlebars Spy user was found in the index.html response');
   t.end();
   });
 });
