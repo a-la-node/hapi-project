bbf929b86929774361fbabc105ee3ea8a1b8e640
Cleop <cleo.pearson@hotmail.co.uk>
2016-11-22 11:22:02 +0000

    Tests that server responds with index.html file

--- tests/server.test.js
+++ tests/server.test.js
@@ -12,3 +12,15 @@ test('Check the index route', t => {
     t.end();
   });
 });
+
+test('Check the response content is received from the server', t => {
+  var options = {
+    method: 'GET',
+    url:'/'
+  };
+  server.inject(options, (response) =>{
+  t.equal(response.statusCode, 200, 'You received a 200 status code');
+  t.notEqual(response.payload.indexOf('<h1> DISGUISED </h1>'), -1, 'The h1 title was found in the index.html response');
+  t.end();
+  });
+});
